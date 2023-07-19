a9bee5b1a727ca35555598bb20b01fd30e0f3389
Stefan Radu <stefan270101@gmail.com>
2023-07-19 18:51:53 +0300

    done lab1

---
+++ init.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+
+export CROSS_COMPILE=/home/stef/Code/arm-dev/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+export BL31=./imx-atf/build/imx8mq/release/bl31.bin
